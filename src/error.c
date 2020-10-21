c41bc1136552c58a603baacf0a0dcee35d3bcdb4
pulk66-s <hugo.marrasse@epitech.eu>
2020-10-21 14:06:42 +0200

    add of error management

--- src/error.c
+++ src/error.c
@@ -5,16 +5,46 @@
 ** error file
 */
 
+#include <unistd.h>
+
+static int is_nb_or_op(char c)
+{
+    if ((c < '0' || c > '9') && c != '+' && c != '-' && c != '*' &&
+            c != '/'&& c != '%') {
+        write(2, "wrong input\n", 12);
+        return (84);
+    }
+    return (1);
+}
+
+static int good_parantheses(char *str)
+{
+    int nb_p_open = 0;
+    int nb_p_closed = 0;
+
+    for (int i = 0 ; str[i] != '\0' ; i++) {
+        if (str[i] == '(')
+            nb_p_open++;
+        if (str[i] == ')')
+            nb_p_closed++;
+    }
+    return (nb_p_open == nb_p_closed ? 1 : 84);
+}
+
 int error(int argc, char **argv)
 {
-    if (argc != 2)
+    if (argc != 2) {
+        write(2, "wrong nb of parameters\n", 12);
         return (84);
+    }
     for (int i = 0 ; argv[1][i] != '\0' ; i++) {
-        if (argv[1][i] < '0' || argv[1][i] > '9') {
-            if (argv[1][i] != '+' && argv[1][i] != '-' && argv[1][i] != '/' && argv[1][i] != '%' && argv[1][i] != ' ') {
-                return (84);
-            }
+        if (is_nb_or_op(argv[1][i]) == 84) {
+            return (84);
         }
     }
-    return (83);
+    if (good_parantheses(argv[1]) == 84) {
+        write(2, "wrong nb of parantheses\n", 25);
+        return (84);
+    }
+    return (1);
 }
--- src/parsing/parse.c
+++ src/parsing/parse.c
@@ -8,19 +8,8 @@
 #include "../include/include.h"
 #include <stdio.h>
 
-static int is_nb(char *str)
-{
-    for (int i = 0 ; str[i] != '\0' ; i++) {
-        if (str[i] < '0' || str[i] > '9') {
-            return (0);
-        }
-    }
-    return (1);
-}
 
 void parse(char *str)
 {
-    while (is_nb(str) == 0) {
-        str = parse_bracket(str);
-    }
+
 }
